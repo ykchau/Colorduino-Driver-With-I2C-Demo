fe75aea057366dc86fe5aae39396e0be2f0dce4f
ykchau <baggio_jr@yahoo.com>
2024-04-08 17:50:26 +0800

    Update font.h

--- Slave/font.h
+++ Slave/font.h
@@ -1,8 +1,8 @@
 /*
-	  Project  : Colorduino
+	Project  : LaLiMat project (https://www.youtube.com/playlist?list=PLJBKmE2nNweRXOebZjydkMEiq2pHtBMOS in Chinese)
    	file     : font.h
-	  Author   : ykchau
- 	  youtube  : youtube.com/ykchau888
+	Author   : ykchau
+ 	youtube  : youtube.com/ykchau888
   	Licenese : GPL-3.0
    	Please let me know if you use it commercial project.
 */
